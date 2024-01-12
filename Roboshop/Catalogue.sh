b0c1a0fc1049c3be1e2c6c62a5e983dc387d3cb8
shaikabd <abcd123@gmail.com>
2024-01-12 15:15:37 +0530

    Catalogue.sh

--- Roboshop/Catalogue.sh
+++ Roboshop/Catalogue.sh
@@ -7,3 +7,23 @@ echo "You are not the Root user to execute this script"
 else
 echo "You are the root user and good to proceed"
 fi
+
+#colours defining
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+
+#Validatin of command execution and dynamic statements printiing
+validate(){
+    if [ $1 ne 0]
+    then
+    echo -e "$2.......... $R FAILURE/n"
+    exit 2
+    else
+    echo -e "$2.........  $G SUCCESS/n"
+
+}
+
+#Disabling existing nodejs version
+dnf module disable nodejs -y
+validate $? "Disabling existing nodejs version"
